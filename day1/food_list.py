63517a5c2c32dd7d190ff544dfd21b076d050034
v186239 <william.rivera@verizonwireless.com>
2021-12-08 07:38:41 -0800

    File, Function, Exception, show version exc

---
+++ day1/food_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+food_list = ["Chicken", "Beans", "Rice", "Salad"]
+print(food_list)
+food_list.append("avacado")
+food_list.append("pastelles")
+print(food_list)
+
---
+++ day1/mylist.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+food_list = ["Chicken", "Beans", "Rice", "Salad"]
+print(food_list)
